5b875e83a4ca7814e0853175dba302fc5ca5c6d2
hikigaya007 <bijop007@gmail.com>
2024-05-22 00:08:48 +0530

    added type to parameter in verifyToken

--- backend/src/utils/verifyJWT.ts
+++ backend/src/utils/verifyJWT.ts
@@ -1,13 +1,14 @@
 import jwt from "jsonwebtoken";
 import { errorHandler } from "./errorHandler";
+import { NextFunction, Request, Response } from "express";
 
-export const verifyTokens = (req , res , next ) => {
+export const verifyTokens = (req: Request , res:Response , next: NextFunction ) => {
     const token = req.cookies.accessToken;
     console.log(token)
 
     if(!token) return next(errorHandler(401 , "Unauthorized"))
 
-    jwt.verify(token , process.env.JWT_KEY as string , (err ,user) =>{
+    jwt.verify(token , process.env.JWT_KEY as string , (err: any ,user: any) =>{
         if(err) return next(errorHandler(403, 'forbidden'))
 
         req.user = user;
